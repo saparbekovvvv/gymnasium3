b83ec1c715ed0b7c147fe95dcaa36e865edc0bd8
rahmanovdev <jumadilrahmonov@gmail.com>
2024-09-11 19:25:47 +0600

    config mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
   images: {
-    domains: ["3-gymnasium.kg"],
+    domains: [
+      "gimnasium.onrender.com",
+      "vermojctkdkrdsxvauwc.supabase.co",
+      "3-gymnasium.kg",
+    ],
   },
-=======
-    images: {
-        domains: ["gimnasium.onrender.com", "vermojctkdkrdsxvauwc.supabase.co"],
-    },
->>>>>>> 505db0dbc6b81c6275754d7ab19a29e82afb0f9c
 };
 
 export default nextConfig;
