bf7187b9b47bd2973667746ab9c6d28e832d4010
nurmuhammetjs13 <nurmuhammedaltybaev@gmail.com>
2024-09-22 14:50:48 +0600

    finished work

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"0rYOYOO9Qk3nrmCMIdxZYWYrpsL6wvBceRDZJGVKF9o=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"nazERdDdb7nGCwFmTB+Budawn8L6V8pZ6M/yQM1kulU=\"\n}"
