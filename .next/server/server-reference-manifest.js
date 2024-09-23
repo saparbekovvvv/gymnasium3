b7c86552a29c48414c138d90fab6e9f15eedc0c4
rahmanovdev <jumadilrahmonov@gmail.com>
2024-09-23 12:41:36 +0600

    ll

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"fs5zsNenpZOqEPdnj/Q48luzDE5tc06sm84VxX2Dq/k=\"\n}"
-=======
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"damFxv7dRJmS+pTO8wOjDjnJ23IYk4ls12esNMQtJbQ=\"\n}"
->>>>>>> 7bb31f12415188117481912368d43c18ac312b89
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"q5nbmwei6wJ+YO2Kdo4HsuYCQOB817a/uucqqb4I4P4=\"\n}"
