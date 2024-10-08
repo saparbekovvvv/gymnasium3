4e8d58c41a48a7ac1c3301bcd6670e64cfd27cb9
nurmuhammetjs13 <nurmuhammedaltybaev@gmail.com>
2024-10-08 13:43:14 +0600

    pulled Nurchiuk with selcetion

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"kIh5Q982FjeZMSWppS4Bi89linO0a7FmiY7RPUw+Li4=\"\n}"
-=======
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"oDlnEV3G2+sOCzwonyRow405NcdCaA8F6FUHwL1DSlQ=\"\n}"
->>>>>>> 00d085d9426644f45a738cbb615f4913c2feb557
-=======
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Ju8T0SoCfOX93VYrhg7So2/90yQOmc7W9LsxeOzza/g=\"\n}"
->>>>>>> d36b2f1b2219903393b9732409cdbbadd32495c5
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"juO8q2tIAjo+eaMWYVkRJAnob1IJWlmdbC16B8uOmbg=\"\n}"
